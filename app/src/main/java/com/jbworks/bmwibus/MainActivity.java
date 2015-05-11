e587f2cecf75c76890e456c51305fd602311c56f
Joe Burke <joe@clinkle.com>
2015-05-10 22:40:12 -0700

    added serial to usb library and example

--- app/src/main/java/com/jbworks/bmwibus/MainActivity.java
+++ app/src/main/java/com/jbworks/bmwibus/MainActivity.java
@@ -8,6 +8,43 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    /*
+    Names:
+    BMW IBUS
+    Android to BMW IBUS
+    AndroidIBus
+    AndroidUsbIBus
+    BMW Droid
+    bmwibus
+    iBusRadio
+    iBusDroid
+
+    this uses a usb to serial interface to connect to the bmw ibus (only the one I use)
+
+    it needs to essentially be a replacement for the head unit. because the head unit will not be
+    accessable once installed.
+
+    It also needs to be able to access these commads from outside the app so it can integrate with
+    tasker or other apps like gesture control
+     */
+
+    /*
+    TASKS
+
+    -USB to Serial
+
+    -IBUS Wrapper
+
+    -Launch on start & Notification status bar
+
+    -Internal Controls
+
+    -External Controls (use shortcuts)
+
+    -Good Frontend UI
+
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
